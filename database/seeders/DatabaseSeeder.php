7f09b7f78ae7734d72d76813bbaa6735a78fa8aa
Parwati Chalise <parwatichalise112@gmail.com>
2024-11-13 12:15:19 +0545

    Added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();     
+         
+         User::create([
+            'firstname' => 'Admin',
+            'lastname'=>'Ad',
+            'email' => 'admin@gmail.com',
+            'username'=>'admin',
+            'contact'=>'9845012345',
+            'password' => bcrypt('123456'), 
+            'role' => 'admin', 
+        ]);
+
+        
+        User::create([
+            'firstname' => 'User',
+            'lastname'=>'Ur',
+            'email' => 'user@gmail.com',
+            'username'=>'user',
+            'contact'=>'9845098765',
+            'password' => bcrypt('123456'), 
+            'role' => 'user', 
+        ]);
+
+        User::create([
+            'firstname' => 'Teacher',
+            'lastname' => 'Tch',
+            'email' => 'teacher@gmail.com',
+            'username' => 'teacher',
+            'contact' => '9845045678',
+            'password' => bcrypt('123456'),
+            'role' => 'teacher',
+        ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,36 +12,6 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-         
-         User::create([
-            'firstname' => 'Admin',
-            'lastname'=>'Ad',
-            'email' => 'admin@gmail.com',
-            'username'=>'admin',
-            'contact'=>'9845012345',
-            'password' => bcrypt('123456'), 
-            'role' => 'admin', 
-        ]);
-
         
-        User::create([
-            'firstname' => 'User',
-            'lastname'=>'Ur',
-            'email' => 'user@gmail.com',
-            'username'=>'user',
-            'contact'=>'9845098765',
-            'password' => bcrypt('123456'), 
-            'role' => 'user', 
-        ]);
-
-        User::create([
-            'firstname' => 'Teacher',
-            'lastname' => 'Tch',
-            'email' => 'teacher@gmail.com',
-            'username' => 'teacher',
-            'contact' => '9845045678',
-            'password' => bcrypt('123456'),
-            'role' => 'teacher',
-        ]);
     }
 }
